d9714c846784f5eed74cd77f354cd68a68736fd9
Axen Georget <mail@axen.io>
2021-03-19 13:19:59 +0100

    feat: create a separated docker-compose file for dev environment

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose -f dc_main.yml -f dc_keycloak.yml down
-docker-compose -f dc_main.yml -f dc_keycloak.yml up -d --build
+docker-compose -f dc_main.yml -f dc_keycloak_prod.yml down
+docker-compose -f dc_main.yml -f dc_keycloak_prod.yml up -d --build
--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker-compose -f dc_main.yml -f dc_keycloak.yml down
+docker-compose -f dc_main.yml -f dc_keycloak_prod.yml down
 docker volume rm portfolio-dashboard_dist-volume || true
