cc4d2329130185aa5ddfa3d1b82935316d8e46d4
Axen Georget <mail@axen.io>
2021-04-20 20:42:24 +0200

    fix: delete trash file

--- env_vars.sh
+++
@@ -1,8 +0,0 @@
-export PD_BACKEND_ADDRESS=http://localhost/api
-export PD_DOMAIN=none
-export PD_HTTPS=false
-export PD_KEYCLOAK_ADDRESS=http://localhost/auth
-export PD_KEYCLOAK_FRONTEND_CLIENT_ID=portfolio-dasbhoard_frontend
-export PD_KEYCLOAK_PASSWORD=admin
-export PD_KEYCLOAK_REALM=portfolio-dasbhoard_realm
-export PD_KEYCLOAK_USER=admin
